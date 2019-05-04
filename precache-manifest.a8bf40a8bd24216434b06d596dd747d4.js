6326264f7699dd2a14f6fbc55d23c9089cf6f401
Duc-GoBear <duc@gobear.com>
2019-05-04 11:44:35 +0700

    Updates

---
+++ precache-manifest.a8bf40a8bd24216434b06d596dd747d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wedding-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "59b2aba30c06b9eec487",
+    "url": "/wedding-app/static/css/main.5b5b1333.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/wedding-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fa9f1e6d5d0e1cd6de56",
+    "url": "/wedding-app/static/js/1.fa9f1e6d.chunk.js"
+  },
+  {
+    "revision": "ce7d8f941001485a6ee4",
+    "url": "/wedding-app/static/js/runtime~main.ce7d8f94.js"
+  },
+  {
+    "revision": "f8a45f753e2316b427c63d8f16bee94b",
+    "url": "/wedding-app/static/media/background.f8a45f75.jpg"
+  },
+  {
+    "revision": "59b2aba30c06b9eec487",
+    "url": "/wedding-app/static/js/main.59b2aba3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wedding-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wedding-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wedding-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fa9f1e6d5d0e1cd6de56",
+    "url": "/wedding-app/static/css/1.98de6d65.chunk.css"
+  },
+  {
+    "revision": "897fabc72ff24d9da8bdd75da97da3bc",
+    "url": "/wedding-app/index.html"
+  }
+];
