cb7523eacc4fdeaaddb56e72404487b53d304ad5
Duc-GoBear <duc@gobear.com>
2019-06-11 16:02:56 +0700

    Updates

---
+++ precache-manifest.091d98aa98d3b00719ceedfea63bad6e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f089b5c09bd0125be2198d1db33b8cda",
+    "url": "/static/media/gallery2.f089b5c0.png"
+  },
+  {
+    "revision": "c6bf0307a1bbe97010e1",
+    "url": "/static/css/main.edd3aed4.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "6b53211225c5dd929f83",
+    "url": "/static/js/1.6b532112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6cfa2817ba609fd307d5f503babb75b",
+    "url": "/static/media/middle.c6cfa281.png"
+  },
+  {
+    "revision": "803be7c69b37349b1a8d7704982f722f",
+    "url": "/static/media/mini4.803be7c6.png"
+  },
+  {
+    "revision": "4285b2a4a7e03cb39f4867aa922067e1",
+    "url": "/static/media/mini3.4285b2a4.png"
+  },
+  {
+    "revision": "1dfc43495199222e1aecf922f1300900",
+    "url": "/static/media/gallery1.1dfc4349.png"
+  },
+  {
+    "revision": "c6bf0307a1bbe97010e1",
+    "url": "/static/js/main.c6bf0307.chunk.js"
+  },
+  {
+    "revision": "13839b417e25eed85a57aec240a52959",
+    "url": "/static/media/gallery4.13839b41.png"
+  },
+  {
+    "revision": "620f8ea992391ac5a3d406305379b2a2",
+    "url": "/static/media/background.620f8ea9.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "6b53211225c5dd929f83",
+    "url": "/static/css/1.7f14fdbf.chunk.css"
+  },
+  {
+    "revision": "c51b2847a8da6231932b0c5ce2090433",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wedding-app/precache-manifest.1881248c86e71f52eaf650a00f91aef0.js"
+  "/precache-manifest.091d98aa98d3b00719ceedfea63bad6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/wedding-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
